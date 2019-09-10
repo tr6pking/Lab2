a9fefb90fbba5231475a266eab6278a58afa9a5a
tr6pking <46894929+tr6pking@users.noreply.github.com>
2019-09-10 12:11:51 -0500

    Add files via upload

--- Project/Data Structures/RecursiveMatlib.java
+++ Project/Data Structures/RecursiveMatlib.java
@@ -3,6 +3,13 @@ public class RecursiveMatlib extends Mathlib{
 
 	@Override
 	public int gcd(int x, int y) {
+		if (y==0) {
+			return x;
+		}
+		if (x >= y && x!=0)
+		{
+			return gcd(x,y % y);
+		}
     System.out.println("Recursive GCD not implemented");
 	return 0;
 	}
